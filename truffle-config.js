5f8d87296badbf092f551d89df222004df81edfc
AhhEagle <dim2though@gmail.com>
2021-06-15 14:09:53 +0100

    add rinkeby configuration

--- truffle-config.js
+++ truffle-config.js
@@ -22,11 +22,12 @@
  *
  */
 
-// const HDWallet = require('truffle-hdwallet-provider');
-// const infuraKey = "fj4jll3k.....";
+ const HDWallet = require('truffle-hdwallet-provider');
+const infuraKey = "4d0576bd55974d68993a3b9c98bc49b3";
+//https://rinkeby.infura.io/v3/4d0576bd55974d68993a3b9c98bc49b3
 //
 // const fs = require('fs');
-// const mnemonic = fs.readFileSync(".secret").toString().trim();
+ const mnemonic = "ginger trial march rubber feature leg level obtain frog corn arctic nature"
 
 module.exports = {
   /**
@@ -51,6 +52,12 @@ module.exports = {
       port: 7545,            // Standard Ethereum port (default: none)
       network_id: "*",       // Any network (default: none)
     },
+    rinkeby: {
+      provider: () => new HDWalletProvider(mnemonic, `https://rinkeby.infura.io/v3/${infuraKey}`),
+      network_id:"4",
+      gas: 4500000,
+      gasPrice: 10000000000 
+    }
 
     // Another network with more advanced options...
     // advanced: {
